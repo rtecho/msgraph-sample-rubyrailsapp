574d8e1af8bff34d182557158b7eb642257afafc
Ratsakon Techo <116138495+rtecho@users.noreply.github.com>
2023-07-08 20:38:05 +0700

    Update oauth_environment_variables.rb

--- graph-sample/config/oauth_environment_variables.rb
+++ graph-sample/config/oauth_environment_variables.rb
@@ -1,1 +1,3 @@
-
+ENV['AZURE_APP_ID'] = 'e22dddc0-818d-4cfb-9262-3a11b71d356a'
+ENV['AZURE_APP_SECRET'] = 'xcK8Q~CsCEROB3TrNK1XVjTfvEa~27c6zugzwcxg'
+ENV['AZURE_SCOPES'] = 'openid profile email offline_access user.read mailboxsettings.read calendars.readwrite'
